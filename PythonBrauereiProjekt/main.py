ff65b591dfc05cb4a3056dcba1d9e92c2a457ff6
mvx-320 <111756622+mvx-320@users.noreply.github.com>
2022-10-26 10:19:40 +0200

    Letztes mal vor Thread umbau (zu myThread) Programm stürzt ohne Fehlermeldung ab Mutex auskommentiert
    (letzter Durchlauf 4min; Lüfter fährt hoch)
    Process finished with exit code -1073740940 (0xC0000374)

--- PythonBrauereiProjekt/main.py
+++ PythonBrauereiProjekt/main.py
@@ -1,30 +1,34 @@
+import datetime
 import sys
 import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from threading import Thread, Timer
-from datetime import datetime
+from datetime import datetime, timedelta
 
-import main # Braucht man für currentID
+import main  # Braucht man für currentID
 import ui1, dialogRast
+
 
 # Github Upload:
 # neues Commit erstellen auf Git->Commit...
 # Push (oben rechts) von ThisIsMyBranch -> origin, master
 
+# FEHLERBEHEBUNG:
+# thread handling + sync/lock (volatile gibt es nicht)
 
-#!!! Editierbarkeit bei allen Items wie bei Item 0,0
-#!!! Typ entfernen start nach Temperaturannäherung in Einstellungen bearbeitbar
-#!!! QProgressBar in QTableWidget mit .setCellWidget()
+# !!! Editierbarkeit bei allen Items wie bei Item 0,0
+# !!! Typ entfernen start nach Temperaturannäherung in Einstellungen bearbeitbar
+# !!! QProgressBar in QTableWidget mit .setCellWidget()
+# !!! Verhindern das man Knöpfe zu häufig drücken kann (oder Arbeitsspeicherüberwachung)
 
 # Rast Hinzufügen Button gedrückt
 def rastHinzufuegen():
-
-    main.currentID = ui.rastenTabelle.rowCount() +1
+    main.currentID = ui.rastenTabelle.rowCount() + 1
     dialog.positionCoB.clear()
     for i in range(main.currentID):
-        dialog.positionCoB.addItem(str(i+1))
-    dialog.positionCoB.setCurrentIndex(main.currentID -1)
+        dialog.positionCoB.addItem(str(i + 1))
+    dialog.positionCoB.setCurrentIndex(main.currentID - 1)
     dialog.bezeichnungLE.clear()
     dialog.temperaturLE.clear()
     dialog.dauerLE.clear()
@@ -35,10 +39,12 @@ def rastHinzufuegen():
 
     NeueRastHinzufuegen.show()
 
-    #!!!!!!! Abchecken ob man neues VericalItem erzeugen muss // Bisher nicht
+    # !!!!!!! Abchecken ob man neues VericalItem erzeugen muss // Bisher nicht
+
 
 # Rast Enfernen Button gedrückt
 def rastEntfernen():
+    # timer.resume()
     if len(rasten_array) > 0:
         loeschAnfrage = QtWidgets.QMessageBox()
         loeschAnfrage.setWindowTitle('Rast löschen?')
@@ -50,6 +56,7 @@ def rastEntfernen():
         # loeschAnfrage.setInformativeText('(Rast wird dauerhaft gelöscht)')
         loeschAnfrage.accepted.connect(Rast.rastLoeschen)
         msg = loeschAnfrage.exec_()
+
 
 class Dialog:
     @staticmethod
@@ -76,20 +83,22 @@ class Dialog:
         if main.currentID < 1:
             main.currentID = 1
 
-        rasten_array.insert(main.currentID - 1, Rast(bezeichnung, str(kommentar), temperatur, dauer, typ, ruehrwerk, brauruf))
+        rasten_array.insert(main.currentID - 1,
+                            Rast(bezeichnung, str(kommentar), temperatur, dauer, typ, ruehrwerk, brauruf))
 
         Rast.tabelleAktualisieren()
+
 
 class Rast:
     def __init__(self, name: str, comment: str, temp: int, time: int, typ: int, agitator: bool, brauruf: bool):
         self.name = str(name)
         self.temp = temp
-        self.time = time
+        self.time = time  # Hier stimmt was nicht
+
         self.typ = typ
         self.agitator = agitator
         self.brauruf = brauruf
-        self.comment = str(comment) #!!!evtl. .adjustSize-Methode bei Comment Item
-
+        self.comment = str(comment)  # !!!evtl. .adjustSize-Methode bei Comment Item
 
     @staticmethod
     def tabelleAktualisieren():
@@ -99,7 +108,7 @@ class Rast:
             ui.rastenTabelle.setRowHeight(i, 50)
 
         for h in range(0, len(rasten_array)):
-            print(h)
+            # print(h)
             for i in range(ui.rastenTabelle.columnCount()):
                 item = QtWidgets.QTableWidgetItem()
 
@@ -154,7 +163,7 @@ class Rast:
                         item.setBackground(brush)
                         item.setText(_translate("MainWindow", "AUS"))
                     else:
-                        print('kein Boolwert')
+                        print('FEHLER: kein Boolwert')
 
                 ui.rastenTabelle.setItem(h, i, item)
 
@@ -173,65 +182,76 @@ class Rast:
         # Ausgangsbeschaltung des Raspberrys
         pass
 
+
 class Runtime:
     @staticmethod
     def start():
-        #!!!hier Visualisierung in den Runmodus setzen
-        #!!!Beim PlayButton bleibt das movieObjekt (propeller) stehen
+        # !!!hier Visualisierung in den Runmodus setzen
 
         threads = []
 
-        # Threads in threads-Array und starten
+        ########Threads in threads-Array und starten
         threadTime = Thread(target=Runtime.timerRun, name="zeit", args=['abschalten'])
-        threadPID = Thread(target=Runtime.pidRegelung, name="pid", args=['pid'])
-
+        threadPID = Thread(target=Runtime.pidRegelung, name="pid", args=['pidRegler'])
+        print("Threads werden erzeugt")
         threads.append(threadTime)
         threadTime.start()
-        #Hier dazwischen darf nichts stehen???
+        ##Hier dazwischen darf nichts stehen???
         threads.append(threadPID)
         threadPID.start()
-        print("Vor join")
-        # Wait for all threads to finish
-        for t in threads:
-            t.join()
-        print("Nach join")
-
-        del threadPID
-        del threadTime
 
     @staticmethod
     def pause():
-        pass
+        timer.pause()
 
     @staticmethod
     def stopp():
         pass
+#voletile
+    @staticmethod  # Thread
+    def timerRun(name):#
+        print(name + '-Thread start')
+        fehler = 0
+        steps = 1000
+        #   try:
+        timer.start()
+        #Erzeugung aktuelleZeit & gesamtZeit
+        ges_zeit = 0
+        for rasten in rasten_array:
+            ges_zeit += int(rasten.time)
+        fehler += 1
+        # Rast.tabelleAktualisieren() #!!! Hier noch das aussehen der Rastentabelle ändern
+        gesamtZeit = timedelta(minutes=float(ges_zeit))
+        aktuelleZeit = timedelta(minutes=float(rasten_array[0].time))  # !!! Darf nicht einfach index 0 haben
 
-    @staticmethod # Thread
-    def timerRun(name):
-        try:
-            print('timer Run ausgeführt')
-            round = 0
-            print("Anzahl der Rast-Objekte im rasten_array: " + str(len(rasten_array)))
+        ui.progressBarGesamt.setMaximum(steps)
+        fehler += 1
 
-            while round < len(rasten_array):
+        fehler += 1
+        # ui.progressBarRast.setMaximum(Runtime.time_to_num(str(aktuelleZeit)))
 
-                time.sleep(5)
-                print('Runde ' + str(round) + ': wurde beendet.')
-                round += 1
+        while timer.get() < aktuelleZeit:
+            # time.sleep(0.5)
+            # print('gesamtZeit: ' + str(gesamtZeit) + '; aktuelleZeit: ' + str(aktuelleZeit) + '; timer: ' + str(timer.get()))
+            #balken = timer.get() / gesamtZeit
+            #balkenInt = int(balken * steps)
+            # ui.progressBarGesamt.setValue(balkenInt)
 
+            ui.sollTemp.setText(str(timer.get()))
+            # print(Runtime.time_to_num(str(timer.get())))
+            # ui.progressBarRast.setValue(Runtime.time_to_num(str(timer.get())))
 
-            time.sleep(4)
-            Rast.tabelleAktualisieren()
+    #      except Exception as e:  # work on python 3.x
+    #         print('Failed by ' + str(fehler) + ': ' + str(e))
 
-        except Exception as e:  # work on python 3.x
-            print('Failed to upload to ftp: ' + str(e))
-
-
-    @staticmethod # Thread
+    @staticmethod  # Thread
     def pidRegelung(name):
-        print(name + ' letz go')
+        print(name + '-Thread start')
 
+    @staticmethod
+    def time_to_num(time_str):
+        hh, mm, ss = map(int, time_str.split(':'))
+        return ss + 60 * (mm + 60 * hh)
 
 
 # Hier ist noch kein Objekt instanziiert
@@ -244,75 +264,105 @@ class MyTimer():
     """
 
     def __init__(self):
-        print('Initializing timer')
+        # print('Initializing timer')
+        #self._mutex = QtCore.QMutex()
         self.timestarted = None
         self.timepaused = None
         self.paused = False
 
     def start(self):
-        """ Starts an internal timer by recording the current time """
+        #if self._mutex.tryLock() == True:
+            #print("Mutex wird gelockt")
+            #self._mutex.lock()
+        #""" Starts an internal timer by recording the current time """
         print("Starting timer")
         self.timestarted = datetime.now()
+        #self._mutex.unlock()
+        #print("Mutex wird unlockt")
 
     def pause(self):
-        """ Pauses the timer """
+        #if self._mutex.tryLock() == True:
+            #print("Mutex wird gelockt")
+            #self._mutex.lock()
+        #""" Pauses the timer """
         if self.timestarted is None:
-            raise ValueError("Timer not started")
+            print("Timer not started")
+            #raise ValueError("Timer not started")
         if self.paused:
-            raise ValueError("Timer is already paused")
+            print("Timer is already paused")
+            #raise ValueError("Timer is already paused")
         print('Pausing timer')
         self.timepaused = datetime.now()
         self.paused = True
+        #self._mutex.unlock()
+        #print("Mutex wird unlockt")
 
     def resume(self):
-        """ Resumes the timer by adding the pause time to the start time """
+        #if self._mutex.tryLock() == True:
+            #print("Mutex wird gelockt")
+            #self._mutex.lock()
+        #""" Resumes the timer by adding the pause time to the start time """
         if self.timestarted is None:
-            raise ValueError("Timer not started")
+            print("Timer not started")
+            #raise ValueError("Timer not started")
         if not self.paused:
-            raise ValueError("Timer is not paused")
+            print("Timer is not paused")
+            #raise ValueError("Timer is not paused")
         print('Resuming timer')
         pausetime = datetime.now() - self.timepaused
         self.timestarted = self.timestarted + pausetime
         self.paused = False
+        #self._mutex.unlock()
+        #print("Mutex wird unlockt")
 
     def get(self):
+        #if self._mutex.tryLock() == True:
+            #print("Mutex wird gelockt")
+            #self._mutex.lock()
         """ Returns a timedelta object showing the amount of time
             elapsed since the start time, less any pauses """
-        print('Get timer value')
+        # print('Get timer value')
         if self.timestarted is None:
-            raise ValueError("Timer not started")
+            print("Timer not started")
+            #raise ValueError("Timer not started")
         if self.paused:
             return self.timepaused - self.timestarted
         else:
             return datetime.now() - self.timestarted
+        #self._mutex.unlock()
+        #print("Mutex wird unlockt")
+
 
 def changeMainWindow():
-#!w!!! Bearbeitbarkeit der rastenTabelle entwernen
-#!w!!! rastenTabelle bearbeiten mit auswählen und dann vorausgewähltem Feld im Dialogfenster
+    # !w!!! Bearbeitbarkeit der rastenTabelle entwernen
+    # !w!!! rastenTabelle bearbeiten mit auswählen und dann vorausgewähltem Feld im Dialogfenster
     ui.rastenTabelle.setColumnWidth(0, 300)
     ui.rastenTabelle.setColumnWidth(1, 50)
     ui.rastenTabelle.setColumnWidth(2, 60)
     ui.rastenTabelle.setColumnWidth(6, 1000)
+    ui.progressBarRast.setFixedHeight(10)
+    ui.progressBarGesamt.setFixedHeight(20)
 
     ui.meldungRuehrwerk.setMovie(movie)
     movie.start()
 
+
 def changeDialog():
-#!!!!! Größe vom Dialogfenster wieder veränderbar machen // evtl noch kleiner
+    # !!!!! Größe vom Dialogfenster wieder veränderbar machen // evtl noch kleiner
     dialog.positionCoB.setCurrentIndex(ui.rastenTabelle.rowCount())
-#!!!!! Die aktuellen Lehrzeichen müssen entfernt werden // evtl schon draußen
+    # !!!!! Die aktuellen Lehrzeichen müssen entfernt werden // evtl schon draußen
     dialog.temperaturLE.setAlignment(QtCore.Qt.AlignCenter)
-    dialog.temperaturLE.setInputMask("00") #D damit die Zahl >0 sein muss
+    dialog.temperaturLE.setInputMask("00")  # D damit die Zahl >0 sein muss
     dialog.dauerLE.setAlignment(QtCore.Qt.AlignCenter)
     dialog.dauerLE.setInputMask("000")
 
+
 def programmVerlassen():
     print('Beim Schließen nachfragen')
-    app.exec_()
+    return app.exec_()
+
 
 if __name__ == "__main__":
-
-
     currentID = 0
     rasten_array = []
 
@@ -321,7 +371,7 @@ if __name__ == "__main__":
     ui = ui1.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
-    movie = QtGui.QMovie("Icons\\Propeller2.gif") # nicht in Methode, weil soll noch aufgerufen werden
+    movie = QtGui.QMovie("Icons\\Propeller2.gif")  # nicht in Methode, weil soll noch aufgerufen werden
     movie.setScaledSize(QtCore.QSize(100, 100))
 
     changeMainWindow()
@@ -330,24 +380,22 @@ if __name__ == "__main__":
     ui.buttonPlay.clicked.connect(Runtime.start)
     ui.buttonPause.clicked.connect(Runtime.pause)
     ui.buttonStop.clicked.connect(Runtime.stopp)
-    #!!! noch in Rast Klasse verschieben
+    # !!! noch in Rast Klasse verschieben
     ui.hinzufuegenB.clicked.connect(rastHinzufuegen)  # .connect darf nicht in "changeMainWindow()" verschoben werden
     ui.entfernenB.clicked.connect(rastEntfernen)
 
-    # Initialisierung Threads
-
-
+    # Initialisierung Timer
+    timer = MyTimer()
 
     NeueRastHinzufuegen = QtWidgets.QDialog()
     dialog = dialogRast.Ui_NeueRastHinzufuegen()
     dialog.setupUi(NeueRastHinzufuegen)
     changeDialog()
 
-
     dialogC = Dialog()
     dialog.dialogButtons.accepted.connect(lambda: dialogC.dialogAuslesen())
 
     MainWindow.show()
 
-    #!!!!!!! Bedingung für Exit // funktioniert noch nicht
+    # !!!!!!! Bedingung für Exit // funktioniert noch nicht
     sys.exit(programmVerlassen())
